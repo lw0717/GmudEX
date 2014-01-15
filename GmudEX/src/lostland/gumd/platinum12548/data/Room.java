6653ceeff3202906a979b3475c6f9f8049a6b562
12548 <gsc12548@gmail.com>
2014-01-15 10:34:12 +0800

    ,,,,

---
+++ GmudEX/src/lostland/gumd/platinum12548/data/Room.java
@@ -0,0 +1,45 @@
+/**
+ * 安卓白金英雄坛制作组 <p>
+ * 文件名：ItemRoom.java <p>
+ * 创建时间：2014-1-15 上午10:05:27 <p>
+ * 所属项目：GmudEX <p>
+ * @author 12548 <p>
+ */
+package lostland.gumd.platinum12548.data;
+
+/**
+ * 类名：Room <p>
+ * 说明：
+ * @author 12548
+ */
+public class Room {
+
+	private void item(ItemGenerator g)
+	{
+		
+	}
+	
+	private abstract class ItemGenerator {
+		
+	}
+	
+	
+	
+	public abstract class Buff {
+		
+	}
+	
+	public abstract class Trigger {
+		
+	}
+	
+	
+	
+	/**
+	 * 
+	 */
+	public Room() {
+		// TODO 自动生成的构造函数存根
+	}
+
+}
